04ed0e21c390df16afe28e3432ee8cb5f526dc4c
rashalenov-source <rash.alenov@gmail.com>
2025-10-12 22:41:55 +0500

    Update config.js

--- informatika-tests/config.js
+++ informatika-tests/config.js
@@ -1,6 +1,6 @@
 // Конфигурация GitHub API
 const GITHUB_CONFIG = {
-    owner: 'alrasheed',    // Замените на ваш username
+    owner: 'rashalenov-source',    // Замените на ваш username
     repo: 'informatika-tests-results', // Репозиторий для результатов
     token: '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'    // Personal Access Token
 };
