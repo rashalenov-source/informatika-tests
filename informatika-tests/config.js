930397ca29c087a21e46fe86cbeda0a22bc37fcf
rashalenov-source <rash.alenov@gmail.com>
2025-10-12 23:02:26 +0500

    Delete informatika-tests directory

--- informatika-tests/config.js
+++
@@ -1,9 +0,0 @@
-// Конфигурация GitHub API
-const GITHUB_CONFIG = {
-    owner: 'rashalenov-source',    // Замените на ваш username
-    repo: 'informatika-tests-results', // Репозиторий для результатов
-    token: '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'    // Personal Access Token
-};
-
-// ВАЖНО! Не коммитьте этот файл с токеном в публичный репозиторий!
-// Добавьте config.js в .gitignore
